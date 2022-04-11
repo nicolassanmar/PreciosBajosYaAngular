9e34173d99ebae9d517e26a390d5bb42316837c2
Nicolás San Martín <nicolassanmar@gmail.com>
2022-04-10 22:36:39 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   mapboxKey:
     'pk.eyJ1IjoieWFleGlzdGUyIiwiYSI6ImNrOG0zOWxxdjBqYmkzbXB6enc5Y3g4eGgifQ.Y1Y2hy6d_gZarBhM0TKO_g',
-  backendUrl: 'https://pedidosyaapi.herokuapp.com/',
+  backendUrl: 'https://pedidosyaapi.herokuapp.com',
   isheroku: true,
 };
